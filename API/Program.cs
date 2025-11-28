748f373fd8927dde2e12347f492ba92eebd56b1c
Jhonatan Q <jhonatanqsilva@gmail.com>
2025-11-27 21:37:40 -0500

    End of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddAutoMapper(typeof(Application.Core.MappingProfiles).Assembly
 var app = builder.Build(); 
 app.UseCors(x=> x.AllowAnyHeader()
                 .AllowAnyMethod()
-                .WithOrigins("http://localhost:3000", "https://localhost:3001"));
+                .WithOrigins("https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
